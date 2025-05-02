32e7eb1827ff6f91af7a8aac9d4cf9c487eeb460
LE DUY HIEU <hieuititiu21200@gmail.com>
2025-05-02 19:00:12 +0700

    Update connectDB.js

--- nodejs/connectDB.js
+++ nodejs/connectDB.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: 'localhost', 
   user: 'root',      
   password: 'hIEU3153@',
-  database: 'car_fragments', 
+  database: 'car_selling_node', 
   multipleStatements: true 
 });
 
